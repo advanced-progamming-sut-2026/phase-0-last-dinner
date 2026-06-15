62fcc2a013b9c25190ddc28ddc3f89532e59e09f
matin-khorasani <matin.khorasani.p@gmail.com>
2026-06-15 13:54:42 +0330

    implement Position class with x/y coordinates

--- src/untitled/src/model/mechanism/Position.java
+++ src/untitled/src/model/mechanism/Position.java
@@ -5,13 +5,15 @@ public class Position {
     private int y;
 
     public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     public int getX() {
-        return 0;
+        return x;
     }
 
     public int getY() {
-        return 0;
+        return y;
     }
 }
