b329795258c61563549c4126bda46b2ace4ea84c
skysep <skysep999@gmail.com>
2026-06-17 20:29:11 +0330

    miniGameFactory implemention

--- src/untitled/src/model/minigame/MiniGameFactory.java
+++ src/untitled/src/model/minigame/MiniGameFactory.java
@@ -2,6 +2,19 @@ package model.minigame;
 
 public class MiniGameFactory {
     public MiniGame create(MiniGameType type) {
-        return null;
+        switch (type) {
+            case VASEBREAKER:
+                return new VasebreakerMiniGame();
+            case I_ZOMBIE:
+                return new IZombieMiniGame();
+            case BEGHOULED:
+                return new BeghouledMiniGame();
+            case ZOMBOTANY:
+                return new ZombotanyMiniGame();
+            case WALLNUT_BOWLING:
+                return new WallnutBowlingMiniGame();
+            default:
+                return null;
+        }
     }
 }
