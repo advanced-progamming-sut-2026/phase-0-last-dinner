d646175a5e377c890de93cf1d4318d9e895eb9bb
matin-khorasani <matin.khorasani.p@gmail.com>
2026-07-06 21:11:37 +0330

    update TerrainType with SLIPPERY_UP, SLIPPERY_DOWN, LOW_BEACH

--- src/untitled/src/model/mechanism/TerrainType.java
+++ src/untitled/src/model/mechanism/TerrainType.java
@@ -4,6 +4,9 @@ public enum TerrainType {
     CLASSIC,
     WATER,
     GRAVE,
-    FROZEN,
+    FROZEN, // اینو برای زامبی یخ زده توی زمین یخی استفاده میکنیم
+    SLIPPERY_UP, // اینو برای زمین لیز توی زمین یخ زده
+    SLIPPERY_DOWN,
+    LOW_BEACH,
     NECROMANCY
 }
