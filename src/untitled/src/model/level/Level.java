c4d7eb9ca39e1567d6e47b20122396d9eac0ddc1
matin-khorasani <matin.khorasani.p@gmail.com>
2026-07-06 21:28:19 +0330

    update Level to receive Chapter for board building

--- src/untitled/src/model/level/Level.java
+++ src/untitled/src/model/level/Level.java
@@ -1,11 +1,14 @@
 package model.level;
 
+import lombok.Getter;import lombok.Setter;
 import model.Plant;
+import model.chapters.Chapter;
 import model.mechanism.Board;
 import model.mechanism.Wave;
 
 import java.util.List;
-
+@Setter
+@Getter
 public abstract class Level {
     private LevelType levelType;
     private List<Plant> allowedPlants;
@@ -14,10 +17,14 @@ public abstract class Level {
     private boolean started;
     private boolean completed;
 
-    protected Level(LevelType levelType) {
+    protected Level(LevelType levelType, Chapter chapter,
+                    List<Plant> allowedPlants, double baseDifficulty) {
         this.levelType = levelType;
+        this.board = chapter.buildBoard();
+        this.allowedPlants = allowedPlants;
+        this.waves = buildWaves(baseDifficulty);
     }
-
+    protected abstract List<Wave> buildWaves(double baseDifficulty);
     public abstract void start();
 
     public abstract boolean isWinConditionMet();
