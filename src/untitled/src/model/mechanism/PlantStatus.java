75903deab371fe4fca62e2d34d0f778f0ca698d2
matin-khorasani <matin.khorasani.p@gmail.com>
2026-07-07 15:13:14 +0330

    implement PlantStatus data class

--- src/untitled/src/model/mechanism/PlantStatus.java
+++ src/untitled/src/model/mechanism/PlantStatus.java
@@ -1,10 +1,21 @@
 package model.mechanism;
 
-import model.Plant;
-
+import lombok.Getter;import lombok.Setter;import model.Plant;
+@Getter
+@Setter
 public class PlantStatus {
     private Plant plant;
     private int sunCost;
     private boolean available;
     private long remainingCooldownTicks;
+
+    public PlantStatus(Plant plant, boolean available, long remainingCooldownTicks) {
+        this.plant = plant;
+        this.sunCost = plant.getSunCost();
+        this.available = available;
+        this.remainingCooldownTicks = remainingCooldownTicks;
+    }
+    public double getRemainingSeconds() {
+        return remainingCooldownTicks / 10.0;
+    }
 }
